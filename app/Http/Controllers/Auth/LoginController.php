f5d01252791efb61177dad9bcebacfb82b691ecb
elder oliveira <elder.oliveira@mepague.net>
2020-08-30 16:01:25 -0300

    Criando rotas de autenticação com o instagram

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use GuzzleHttp\Client;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectToInstagramProvider()
+    {
+        $appId = config('services.instagram.client_id');
+        $redirectUri = urlencode(config('services.instagram.redirect'));
+        return redirect()->to("https://api.instagram.com/oauth/authorize?client_id={$appId}&redirect_uri={$redirectUri}&scope=user_profile,user_media&response_type=code");
+    }
+
+    public function instagramProviderCallback(Request $request)
+    {
+        $code = $request->code;
+        if (empty($code)) return redirect()->route('home')->with('error', 'Failed to login with Instagram.');
+
+        $appId = config('services.instagram.client_id');
+        $secret = config('services.instagram.client_secret');
+        $redirectUri = config('services.instagram.redirect');
+
+        $client = new Client();
+
+        // Get access token
+        $response = $client->request('POST', 'https://api.instagram.com/oauth/access_token', [
+            'form_params' => [
+                'app_id' => $appId,
+                'app_secret' => $secret,
+                'grant_type' => 'authorization_code',
+                'redirect_uri' => $redirectUri,
+                'code' => $code,
+            ]
+        ]);
+
+        if ($response->getStatusCode() != 200) {
+            return redirect()->route('home')->with('error', 'Unauthorized login to Instagram.');
+        }
+
+        $content = $response->getBody()->getContents();
+        $content = json_decode($content);
+
+        $accessToken = $content->access_token;
+        $userId = $content->user_id;
+
+        // Get user info
+        $response = $client->request('GET', "https://graph.instagram.com/me?fields=id,username,account_type&access_token={$accessToken}");
+
+        $content = $response->getBody()->getContents();
+        $oAuth = json_decode($content);
+
+        // Get instagram user name
+        $username = $oAuth->username;
+
+        // do your code here
+    }
+}
